8c75bb21a07c683665296f09c46067e83078da4b
andrecp <andreprado88@gmail.com>
2014-11-15 12:06:28 -0200

    Working with JSON body retrieved from github webhook

--- webhook/views.py
+++ webhook/views.py
@@ -1,9 +1,15 @@
 from pyramid.response import Response
 from pyramid.view import view_config
-
+import json
 
 @view_config(route_name='root')
 def root_view(request):
-    print 'Hello world!'
-    return Response('Hello world!')
+    if request.method == 'GET':
+        print 'Hello world!'
+        return Response('Hello world!')
+    elif request.method == 'POST':
+        print 'Hello post world!\n'
+        data = request.json_body
+        print 'New commit by: {0} '.format(data['commits'][0]['author']['name'])
+        return Response('Hello post world!')
 
