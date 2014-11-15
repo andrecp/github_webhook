c50b8cd3d147943d34421b0fb1bf3557daaa1741
andrecp <andreprado88@gmail.com>
2014-11-15 11:50:17 -0200

    Adding folder to trigger github callbacks

--- webhook/views.py
+++ webhook/views.py
@@ -4,5 +4,6 @@ from pyramid.view import view_config
 
 @view_config(route_name='root')
 def root_view(request):
-    return Response('Hello s!' % request.matchdict)
+    print 'Hello world!'
+    return Response('Hello world!')
 
