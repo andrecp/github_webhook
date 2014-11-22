2d50920d0573b6db6181acb8af8301dc159dfae7
andrecp <andreprado88@gmail.com>
2014-11-22 14:28:28 -0200

    adding branch verification

--- webhook/views.py
+++ webhook/views.py
@@ -1,15 +1,18 @@
 from pyramid.response import Response
 from pyramid.view import view_config
 import json
+import os
 
 @view_config(route_name='root')
 def root_view(request):
+    print os.environ['WEBHOOK_BRANCH']
     if request.method == 'GET':
         print 'Hello world!'
         return Response('Hello world!')
     elif request.method == 'POST':
         print 'Hello post world!\n'
         data = request.json_body
+        print 'New commit by: {0} '.format(data['commits'][0]['author']['name'])
         print 'New commit by: {0} '.format(data['commits'][0]['author']['name'])
         return Response('Hello post world!')
 
