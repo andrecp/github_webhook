bd59da7de7b5cfe53d675289744ad33f1c1f577e
andrecp <andreprado88@gmail.com>
2014-11-30 21:37:36 -0200

    Adding start_me_up.sh with the necessary ENV, source start_me_up.sh. Getting content from github, work in progress

---
+++ start_me_up.sh
@@ -0,0 +1,4 @@
+export VENV=../venv/bin
+export GIT_URL=https://api.github.com/repos/andrecp/github_webhook/contents/
+export GIT_BRANCH=master
+export PUSH_URL=https://fiery-inferno-4391.firebaseio.com/desk_list.json
--- webhook/views.py
+++ webhook/views.py
@@ -7,42 +7,49 @@ import requests
 
 from engine import get_branch, get_author, get_changes
 
-def sendData(url, data):
+def sendData(git_url, git_branch, push_url, data):
     # send add data
-    data_from_file = open(''.join(data[0]))
-    json_from_file = json.load(data_from_file)
     for new_data in data[0]:
-        requests.post(url+'desk_list.json', json=json_from_file)
+        data_from_github = requests.get(git_url+new_data, params=git_branch)
+        print data_from_github.json()
+        #data_from_file = open(''.join(data[0]))
+        #json_from_file = json.load(data_from_file)
+        #requests.post(push_url, json=json_from_file)
     # send update data
     # send remove data
 
 @view_config(route_name='root', request_method='POST')
 def root_view(request):
-    branch = os.environ.get('WEBHOOK_BRANCH')
-    if branch == None:
-        return Response('WEBHOOK_BRANCH not set')
+    # check for ENV variables
+    git_url = os.environ.get('GIT_URL')
+    if git_url == None:
+        return Response('GIT_URL not set')
+    git_branch = os.environ.get('GIT_BRANCH')
+    if git_branch == None:
+        return Response('GIT_BRANCH not set')
     push_to = os.environ.get('PUSH_URL')
     if push_to == None:
         return Response('PUSH_URL not set')
 
+    # get data from the github event
     data = request.json_body
     author = get_author(data)
     push_branch = get_branch(data)
     changes = get_changes(data)
 
-    if branch == push_branch:
+    if git_branch == push_branch:
         try:
             r = requests.get(push_to)
         except requests.exceptions.ConnectionError as e:
             return Response('Domain does not exist.')
 
         if r.status_code == 200:
-            sendData(push_to, changes)
-            response_msg = '{0}\nSuccessfuly commited to {1}'.format(author,branch)
+            sendData(git_url, git_branch, push_to, changes)
+            response_msg = '{0}\nSuccessfuly commited to {1}'.format(author,git_branch)
         else:
             response_msg = 'Failed to connect to Firebase\nStatus:' + str(r.status_code)
     else:
-        response_msg = '{0} wrong branch!\nYou commited to {1}.\nOnly accepting commits to {2} branch.'.format(author, push_branch, branch)
+        response_msg = '{0} wrong branch!\nYou commited to {1}.\nOnly accepting commits to {2} branch.'.format(author, push_branch, git_branch)
     return Response(response_msg)
         
 @notfound_view_config(request_method='GET')
