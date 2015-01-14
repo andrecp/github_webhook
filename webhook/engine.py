f2a38b325dd7d03f4b91b8f8888d77d3b73919d4
andrecp <andreprado88@gmail.com>
2015-01-14 15:57:41 +0000

    Adding a whitelist for .json files

--- webhook/engine.py
+++ webhook/engine.py
@@ -1,5 +1,6 @@
-import json
 import base64
+import json
+import os
 
 __all__ = [
     'get_branch',
@@ -7,6 +8,10 @@ __all__ = [
     'get_changes',
     'get_github_json',
 ]
+
+DEFAULTS = {
+    'whitelist' : os.environ.get('WEBHOOK_WHITELIST', '.json'),
+}
 
 def get_branch(commit):
     branch = commit['ref'].split('/')
@@ -17,11 +22,27 @@ def get_author(commit):
     author = commit['commits'][0]['author']['name']
     return author
 
+def _whitelist(candidate_list):
+    allowed = DEFAULTS['whitelist']
+    whitelist = []
+
+    for item in candidate_list:
+        file_extension = item.split('.')[-1] 
+        if file_extension == allowed:
+            whitelist.append(item)
+    return whitelist
+
 def get_changes(commit):
     changes_path = commit['commits'][0]
+    
     added = changes_path['added']
     modified = changes_path['modified']
     removed = changes_path['removed']
+    
+    # Get only the whitelist items
+    added = _whitelist(added)
+    modified = _whitelist(modified)
+    removed = _whitelist(removed)
     return (added, modified, removed)
 
 def get_github_json(data):
